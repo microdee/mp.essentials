b0b33cc5dedfb9c244f770a03e908dd7cf29bb12
David Mórász <m@mcro.de>
2017-05-25 03:14:14 +0200

    made LaunchOSK work on windows 10

--- src/windows/WindowsLaunchOSKNode.cs
+++ src/windows/WindowsLaunchOSKNode.cs
@@ -3,7 +3,9 @@ using System;
 using System.ComponentModel.Composition;
 using System.Diagnostics;
 using System.Runtime.InteropServices; //added for keyboard closure
-using System.Windows.Interop; //Keyboard closure - must add reference for WindowsBase
+using System.Windows.Interop;
+using Microsoft.Win32;
+//Keyboard closure - must add reference for WindowsBase
 
 using VVVV.PluginInterfaces.V1;
 using VVVV.PluginInterfaces.V2;
@@ -15,15 +17,55 @@ using VVVV.Core.Logging;
 
 namespace VVVV.Nodes
 {
-	[PluginInfo(
+    internal enum OSVersion
+    {
+        Undefined,
+        Win7,
+        Win8Or81,
+        Win10
+    }
+    internal static class EnvironmentEx
+    {
+        private static OSVersion OSVersion = OSVersion.Undefined;
+
+        internal static OSVersion GetOSVersion()
+        {
+            if (OSVersion != OSVersion.Undefined)
+                return OSVersion;
+
+            string OSName = GetOSName();
+
+            if (OSName.Contains("7"))
+                OSVersion = OSVersion.Win7;
+            else if (OSName.Contains("8"))
+                OSVersion = OSVersion.Win8Or81;
+            else if (OSName.Contains("10"))
+                OSVersion = OSVersion.Win10;
+
+            return OSVersion;
+        }
+
+        private static string GetOSName()
+        {
+            RegistryKey rk = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            if (rk == null) return "";
+            return (string)rk.GetValue("ProductName");
+        }
+    }
+
+    [PluginInfo(
         Name = "LaunchOSK",
         Category = "Windows",
         Author = "microdee",
         AutoEvaluate = true)]
 	public class WindowsLaunchOSKNode : IPluginEvaluate
 	{
-		#region fields & pins
-		[Input("Open")]
+	    private const string TabTipWindowClassName = "IPTip_Main_Window";
+	    private const string TabTipExecPath = @"C:\Program Files\Common Files\microsoft shared\ink\TabTip.exe";
+	    private const string TabTipRegistryKeyName = @"HKEY_CURRENT_USER\Software\Microsoft\TabletTip\1.7";
+
+        #region fields & pins
+        [Input("Open")]
 		public ISpread<bool> FOpen;
 		[Input("Close")]
 		public ISpread<bool> FClose;
@@ -42,14 +84,37 @@ namespace VVVV.Nodes
 
         [DllImport("user32.dll")]
         public static extern IntPtr FindWindow(String sClassName, String sAppName);
-		
-		//open keyboard
-		void openKeyboard()
-		{
-            ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Program Files\Common Files\Microsoft Shared\ink\TabTip.exe");
-            startInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            Process.Start(startInfo);
-		}
+
+	    private static IntPtr GetTabTipWindowHandle() => FindWindow(TabTipWindowClassName, null);
+
+        private static void EnableTabTipOpenInDesctopModeOnWin10()
+	    {
+	        const string TabTipAutoInvokeKey = "EnableDesktopModeAutoInvoke";
+
+	        int EnableDesktopModeAutoInvoke = (int)(Registry.GetValue(TabTipRegistryKeyName, TabTipAutoInvokeKey, -1) ?? -1);
+	        if (EnableDesktopModeAutoInvoke != 1)
+	            Registry.SetValue(TabTipRegistryKeyName, TabTipAutoInvokeKey, 1);
+	    }
+
+        //open keyboard
+        void openKeyboard()
+        {
+            const string TabTipDockedKey = "EdgeTargetDockedState";
+            const string TabTipProcessName = "TabTip";
+
+            int docked = (int)(Registry.GetValue(TabTipRegistryKeyName, TabTipDockedKey, 1) ?? 1);
+            if (docked == 1)
+            {
+                Registry.SetValue(TabTipRegistryKeyName, TabTipDockedKey, 0);
+                foreach (Process tabTipProcess in Process.GetProcessesByName(TabTipProcessName))
+                    tabTipProcess.Kill();
+            }
+
+            if (EnvironmentEx.GetOSVersion() == OSVersion.Win10)
+                EnableTabTipOpenInDesctopModeOnWin10();
+
+            Process.Start(TabTipExecPath);
+        }
 
 		//close keyboard
 		void closeKeyboard()
