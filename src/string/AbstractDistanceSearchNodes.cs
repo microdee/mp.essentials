4ddca1d3d9781a24b6ef9d68adbcfa2c01c883fb
David Mórász <m@mcro.de>
2017-05-25 03:30:47 +0200

    Added nodes to measure string distances
    implementing most algorithms from SimMetrics

---
+++ src/string/AbstractDistanceSearchNodes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimMetrics.Net.API;
+using VVVV.Nodes.PDDN;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    public abstract class AbstractStringDistanceNode<TMetrics> : IPluginEvaluate where TMetrics : AbstractStringMetric, new()
+    {
+        [Input("Input 1")]
+        public ISpread<string> FInput1;
+        [Input("Input 2")]
+        public ISpread<string> FInput2;
+
+        [Output("Similarity")]
+        public ISpread<double> FSim;
+        [Output("Similarity Explained")]
+        public ISpread<string> FSimexplained;
+        [Output("Unnormalized Similarity")]
+        public ISpread<double> FSimUnnorm;
+        [Output("Algorithm")]
+        public ISpread<TMetrics> FAlg;
+
+        [Output("Short Description", Visibility = PinVisibility.Hidden)]
+        public ISpread<string> FShortDesc;
+        [Output("Long Description", Visibility = PinVisibility.Hidden)]
+        public ISpread<string> FLongDesc;
+
+        public virtual void AuxOperation(string a, string b, int i) { }
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (FInput1.IsChanged || FInput2.IsChanged)
+            {
+                this.SetSliceCountForAllOutput(SpreadMax, ignore: new []{ "FShortDesc", "FLongDesc", "FAlg"});
+                var alg = new TMetrics();
+                FAlg[0] = alg;
+                FShortDesc[0] = alg.ShortDescriptionString;
+                FLongDesc[0] = alg.LongDescriptionString;
+
+                for (int i = 0; i < SpreadMax; i++)
+                {
+                    FSim[i] = alg.GetSimilarity(FInput1[i], FInput2[i]);
+                    FSimUnnorm[i] = alg.GetUnnormalisedSimilarity(FInput1[i], FInput2[i]);
+                    try
+                    {
+                        FSimexplained[i] = alg.GetSimilarityExplained(FInput1[i], FInput2[i]);
+                    }
+                    catch { }
+                    AuxOperation(FInput1[i], FInput2[i], i);
+                }
+            }
+        }
+    }
+}
---
+++ src/string/StringDistanceNodes.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimMetrics.Net.Metric;
+using VVVV.PluginInterfaces.V2;
+
+namespace VVVV.Nodes
+{
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "Block",
+        Author = "microdee"
+    )]
+    public class StringBlockDistanceNode : AbstractStringDistanceNode<BlockDistance> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "ChapmanLengthDeviation",
+        Author = "microdee"
+    )]
+    public class StringChapmanLengthDeviationNode : AbstractStringDistanceNode<ChapmanLengthDeviation> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "ChapmanMeanLength",
+        Author = "microdee"
+    )]
+    public class StringChapmanMeanLengthNode : AbstractStringDistanceNode<ChapmanMeanLength> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "CosineSimilarity",
+        Author = "microdee"
+    )]
+    public class StringCosineSimilarityNode : AbstractStringDistanceNode<CosineSimilarity> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "DiceSimilarity",
+        Author = "microdee"
+    )]
+    public class StringDiceSimilarityNode : AbstractStringDistanceNode<DiceSimilarity> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "Euclidean",
+        Author = "microdee"
+    )]
+    public class StringEuclideanDistanceNode : AbstractStringDistanceNode<EuclideanDistance>
+    {
+        [Output("Euclid Distance")]
+        public ISpread<double> FEuclidDist;
+
+        public override void AuxOperation(string a, string b, int i)
+        {
+            FEuclidDist[i] = FAlg[0].GetEuclidDistance(a, b);
+        }
+    }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "JaccardSimilarity",
+        Author = "microdee"
+    )]
+    public class StringJaccardSimilarityNode : AbstractStringDistanceNode<JaccardSimilarity> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "Jaro",
+        Author = "microdee"
+    )]
+    public class StringJaroNode : AbstractStringDistanceNode<Jaro> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "JaroWinkler",
+        Author = "microdee"
+    )]
+    public class StringJaroWinklerNode : AbstractStringDistanceNode<JaroWinkler> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "Levenstein",
+        Author = "microdee"
+    )]
+    public class StringLevensteinNode : AbstractStringDistanceNode<Levenstein> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "MatchingCoefficient",
+        Author = "microdee"
+    )]
+    public class StringMatchingCoefficientNode : AbstractStringDistanceNode<MatchingCoefficient> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "MongeElkan",
+        Author = "microdee"
+    )]
+    public class StringMongeElkanNode : AbstractStringDistanceNode<MongeElkan> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "NeedlemanWunch",
+        Author = "microdee"
+    )]
+    public class StringNeedlemanWunchNode : AbstractStringDistanceNode<NeedlemanWunch> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "OverlapCoefficient",
+        Author = "microdee"
+    )]
+    public class StringOverlapCoefficientNode : AbstractStringDistanceNode<OverlapCoefficient> { }
+
+    [PluginInfo(
+        Version= "Distance",
+        Category = "String",
+        Name = "QGramsDistance",
+        Author = "microdee"
+    )]
+    public class StringQGramsDistanceNode : AbstractStringDistanceNode<QGramsDistance> { }
+
+    [PluginInfo(
+        Version = "Distance",
+        Category = "String",
+        Name = "SmithWaterman",
+        Author = "microdee"
+    )]
+    public class StringSmithWatermanNode : AbstractStringDistanceNode<SmithWaterman> { }
+}
