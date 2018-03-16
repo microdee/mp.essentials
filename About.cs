95813864fb9fda3cce1c389818d39ad73d5754fc
David Mórász <m@mcro.de>
2018-03-16 21:07:39 +0100

    added vvvv DLL's as submodules

---
+++ About.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using VVVV.PluginInterfaces.V2;
+
+namespace mp.essentials
+{
+    [PluginInfo(
+        Name = "About",
+        Category = "mp.essentials",
+        Author = "microdee"
+    )]
+    public class AboutNode : IPluginEvaluate, IPartImportsSatisfiedNotification
+    {
+        [Output("Version")] public ISpread<string> FVer;
+
+        public void Evaluate(int SpreadMax) { }
+        public void OnImportsSatisfied()
+        {
+            FVer[0] = typeof(AboutNode).Assembly.GetName().Version.ToString();
+        }
+    }
+}
