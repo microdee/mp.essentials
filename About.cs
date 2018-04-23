7c429cd7aff8066d211da597e7ae935fd78e26e9
David Mórász <m@mcro.de>
2018-04-23 18:03:38 +0200

    improved About

--- About.cs
+++ About.cs
@@ -1,27 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using md.stdl.Interfaces;
+using mp.pddn;
 using VVVV.PluginInterfaces.V2;
 
 namespace mp.essentials
 {
+    [Startable]
+    public class VersionWriter : IStartable
+    {
+        public static string VersionPath { get; private set; }
+        public static string VvvvDir { get; private set; }
+
+        public void Start()
+        {
+            var ver = typeof(VersionWriter).Assembly.GetName().Version.ToString();
+            VvvvDir = Path.GetDirectoryName(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+            if (VvvvDir != null)
+            {
+                VersionPath = Path.Combine(VvvvDir, "packs", "mp.essentials", "version.info");
+                File.WriteAllText(VersionPath, ver);
+            }
+            else
+            {
+                VersionPath = "null";
+            }
+        }
+
+        public void Shutdown() { Start(); }
+    }
+
     [PluginInfo(
         Name = "About",
-        Category = "mp.essentials",
+        Category = "Mp.Essentials",
         Author = "microdee"
     )]
     public class AboutNode : IPluginEvaluate, IPartImportsSatisfiedNotification
     {
-        [Output("Version")] public ISpread<string> FVer;
+        [Output("mp.essentials Version")] public ISpread<string> FVer;
+        [Output("md.stdl Version")] public ISpread<string> FMdStdlVer;
+        [Output("mp.pddn Version")] public ISpread<string> FMpPddnVer;
+
+        [Output("Version File Path")] public ISpread<string> FVerPath;
+        [Output("VVVV Dir")] public ISpread<string> FVDir;
 
         public void Evaluate(int SpreadMax) { }
         public void OnImportsSatisfied()
         {
             FVer[0] = typeof(AboutNode).Assembly.GetName().Version.ToString();
+            FMdStdlVer[0] = typeof(IMainlooping).Assembly.GetName().Version.ToString();
+            FMpPddnVer[0] = typeof(SpreadWrapper).Assembly.GetName().Version.ToString();
+
+            FVDir[0] = VersionWriter.VvvvDir;
+            FVerPath[0] = VersionWriter.VersionPath;
         }
     }
 }
