5d70c6583673dbac4b4e197df2a24e0dede56b11
David Morasz <m@mcro.de>
2022-05-15 21:56:38 +0200

    + DirectoryExists

---
+++ windows/DirectoryExists.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Linq;
+using mp.pddn;
+using VVVV.PluginInterfaces.V2;
+
+namespace mp.essentials.windows
+{
+    [PluginInfo(
+        Name = "DirectoryExists",
+        Category = "File",
+        Author = "microdee"
+    )]
+    public class DirectoryExists : IPluginEvaluate
+    {
+        [Input("Directory", StringType = StringType.Directory)]
+        public IDiffSpread<string> DirectoryIn;
+        
+        [Input("Update", IsBang = true)]
+        public ISpread<bool> UpdateIn;
+        
+        [Output("Exists")]
+        public ISpread<bool> ExistsOut;
+
+        public void Evaluate(int SpreadMax)
+        {
+            if (DirectoryIn.IsChanged || UpdateIn.TryGetSlice(0))
+            {
+                ExistsOut.AssignFrom(DirectoryIn.Select(Directory.Exists));
+            }
+        }
+    }
+}
