317c8acbaf9d2ec4bd8906d69d8f7a3b31c9c340
David Morasz <m@mcro.de>
2022-05-19 04:09:31 +0200

    TakeUntilNull node can consider more than one zero

--- raw/RawTakeUntilNull.cs
+++ raw/RawTakeUntilNull.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using mp.pddn;
 using VVVV.PluginInterfaces.V2;
 
 namespace mp.essentials.raw
@@ -18,6 +19,8 @@ namespace mp.essentials.raw
     {
         [Input("Input")]
         public IDiffSpread<Stream> FInput;
+        [Input("Null Count")]
+        public IDiffSpread<int> NullCount;
 
         [Output("Output")]
         public ISpread<Stream> FOutput;
@@ -33,17 +36,37 @@ namespace mp.essentials.raw
                 for (int i = 0; i < FInput.SliceCount; i++)
                 {
                     if (FOutput[i] == null) FOutput[i] = new MemoryComStream();
-                    if(FInput[i].Length < FOutput[i].Length) FOutput[i].SetLength(FInput[i].Length);
+                    if (FInput[i].Length < FOutput[i].Length) FOutput[i].SetLength(FInput[i].Length);
                     FInput[i].Position = 0;
                     FOutput[i].Position = 0;
                     int dl = 0;
+                    int desiredNullCount = NullCount[i];
+                    int nullCount = 0;
                     while (true)
                     {
                         int cbyte = FInput[i].ReadByte();
-                        if(cbyte <= 0) break;
+                        if (cbyte < 0)
+                        {
+                            desiredNullCount = 0;
+                            break;
+                        }
+                        if (cbyte == 0)
+                        {
+                            nullCount++;
+                            if (nullCount >= NullCount[i])
+                            {
+                                dl++;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            nullCount = 0;
+                        }
                         FOutput[i].WriteByte((byte) cbyte);
                         dl++;
                     }
+                    dl = dl - desiredNullCount;
                     FOutput[i].SetLength(dl);
                     FDataLength[i] = dl;
                     FInput[i].Position = 0;
